c2c37f9363ef507fef19ce750d9ebb598b25763a
Oscar Thomson <oscmog@gmail.com>
2026-06-19 09:21:57 +0000

    Created basic project template

---
+++ main.js
@@ -0,0 +1,9 @@
+const extractData = require('./methods/extractData');
+
+const data = extractData(process.argv);
+
+// Incriment values and check values against achievements (total number of commands run, times this specific command was run)
+
+// Store stats of command (length, arguments, etc) and check against achievements
+
+// Check command against specific achievement conditions
